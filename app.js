d78e24c7f8b31dd3b5b8d3e45274f35669dbb7a3
sucheng <sc18708965781@163.com>
2020-06-15 21:44:02 +0800

    开放静态资源文件，通过localhost/home/login.html 即可访问

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // 引用express框架
 const express = require('express');
+// 引入path模块
+const path = require('path');
 // 引入路由
 const home = require('./route/home');
 const admin = require('./route/admin');
 
 // 创建网站服务器
 const app = express();
+// 开放静态资源文件
+app.use(express.static(path.join(__dirname,'public')));
 // app.use 拦截所有请求匹配路由
 app.use('/home',home);
 app.use('/admin',admin);
--- route/home.js
+++ route/home.js
@@ -2,6 +2,7 @@
 const express = require('express');
 // 创建博客首页路由
 const home = express.Router();
+
 home.get('/',(req,res)=>{
     res.send('欢迎来到博客首页');
 });
