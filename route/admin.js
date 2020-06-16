4ea2e2d855c088b187c0042a3f06de5542be8b24
sucheng <sc18708965781@163.com>
2020-06-16 09:36:56 +0800

    'admin路由配置'

--- route/admin.js
+++ route/admin.js
@@ -6,9 +6,19 @@ const admin = express.Router();
 admin.get('/login',(req,res) => {
     // res.send('欢迎来到admin页面');
     res.render('admin/login');
-})
+});
 // 用户列表路由 加上模板引擎渲染
 admin.get('/user',(req,res) => {
     res.render('admin/user')
-})
+});
+admin.get('/article-edit',(req,res) => {
+    res.render('admin/article-edit')
+});
+admin.get('/article',(req,res) => {
+    res.render('admin/article')
+});
+admin.get('/user-edit',(req,res) => {
+    res.render('admin/user-edit')
+});
+
 module.exports = admin;
