409989affb389ed3da045ea80b4ed890509bf186
sucheng <sc18708965781@163.com>
2020-06-19 21:02:42 +0800

    验证用户登录功能

--- model/connect.js
+++ model/connect.js
@@ -4,6 +4,8 @@ const options ={
     useNewUrlParser: true ,
     useUnifiedTopology: true
 }
+//弃用警告  百度之后的一个参数
+mongoose.set('useCreateIndex', true) 
 // 连接数据库 无密码链接方式
 mongoose.connect('mongodb://localhost/blog',options)
         .then(() => {console.log('数据库连接成功')})
--- route/admin.js
+++ route/admin.js
@@ -1,5 +1,7 @@
 // 引用express框架
 const express = require('express');
+// 引入集合构造函数
+const {User} = require('../model/user');
 // cosnt 
 // 创建admin路由
 const admin = express.Router();
@@ -21,10 +23,27 @@ admin.get('/article',(req,res) => {
 admin.get('/user-edit',(req,res) => {
     res.render('admin/user-edit')
 });
+// POST请求处理
+admin.post('/login', async function(req, res){
+    // 服务器端处理用户输入方式不能为空
+    const {email ,password} = req.body;
+    // 判断用户输入是否为空
+    if(email.trim().length == 0 || password.trim().length == 0) return res.status(400).render('admin/err.art',{msg:'用户密码错误'});
+    // 查询数据库中是否有用户输入的用户信息 es6语法email：email  可以写成email
+    const user =await User.findOne({email});  
+    // {"state":0,"_id":"5eeb22ca5f699832b0c49b63","username":"sucheng","email":"943704372@qq.com","password":"45784565","role":"admin","__v":0}
+    if(user){
+        // res.send(user);
+        if(user.password == password){
+           res.send('登录成功');
+           return
+        }else{
+            res.status(400).render('admin/err.art',{msg:'用户密码错误'});
+        }
+    }else{
+        res.status(400).render('admin/err.art',{msg:'用户密码错误'});
+    }
 
-admin.post('/login', function(req, res){
-    res.send(req.body);
-    // if(!req.body) return res.sendStatus(400);
-    // res.send('welcome, '+ req.body.email);
+    // res.send('用户登录成功');
 })
 module.exports = admin;
